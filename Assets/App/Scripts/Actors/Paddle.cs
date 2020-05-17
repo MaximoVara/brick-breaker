543da81481476b06d25596c54e3ff8d15b85209f
Kevin Pacheco <k.pacheco@me.com>
2020-05-17 00:13:42 -0700

    Added paddle to scene and collision detection.

---
+++ Assets/App/Scripts/Actors/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+	public float speed = 5.0F;
+
+	private Rigidbody rigidbody;
+	private float xAxis = 0.0F;
+
+	private void Awake() {
+		this.rigidbody = this.GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate() {
+		var velocity = this.rigidbody.velocity;
+
+		velocity.x = this.speed * this.xAxis;
+
+		this.rigidbody.velocity = velocity;
+	}
+
+	private void Update() {
+		this.xAxis = Input.GetAxis("Horizontal");
+	}
+}

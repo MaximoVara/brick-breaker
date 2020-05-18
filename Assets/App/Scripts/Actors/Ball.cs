d8b4370e826bb0bf2765a07dc5084ccfa0264178
WolfpackDragon <maximor.vara@gmail.com>
2020-05-17 21:45:38 -0700

    Paddle + Ball

---
+++ Assets/App/Scripts/Actors/Ball.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private new Rigidbody rigidbody;
+    public float startForce = 25.0F;
+    private UnityEngine.Vector3 lastVelocity;
+    private float lastCollisionTime;
+    private void Awake()
+    {
+        this.rigidbody = this.GetComponent<Rigidbody>();
+    }
+    private void Start()
+    {
+        this.rigidbody.AddForce(UnityEngine.Vector3.forward * startForce, ForceMode.Force);
+        this.lastVelocity = this.rigidbody.velocity;
+    }
+    private void FixedUpdate()
+    {
+        var velocity = this.rigidbody.velocity;
+        var speed = velocity.magnitude;
+        speed = Mathf.Clamp(speed, 15.0f, 20.0F);
+        this.rigidbody.velocity = velocity.normalized * speed;
+        this.lastVelocity = velocity;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        var velocity = this.rigidbody.velocity;
+        
+        var xDiff = Mathf.Abs(velocity.x = this.lastVelocity.x);
+        var zDiff = Mathf.Abs(velocity.z = this.lastVelocity.z);
+
+        if (xDiff < 0.1F || zDiff < 0.1F)
+        {   
+            var angle = Random.Range(-1.0F, 1.5F);
+            lastVelocity = UnityEngine.Quaternion.Euler(0.0F, angle, 0.0F) * velocity;
+        }
+        this.rigidbody.velocity = velocity;
+        
+        Vector3 rando = new Vector3(0.0F, Random.Range(-1.0F, 1.0F));
+        var reflection = Vector3.Reflect(this.lastVelocity.normalized, collision.GetContact(0).normal);
+        this.rigidbody.velocity = reflection * velocity.magnitude + rando;
+    }
+}
---
+++ Assets/App/Scripts/Actors/Paddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed = 5.0F;
+    private Rigidbody rigidBody;
+    private float xAxis = 0.0F;
+    
+    private void Awake(){
+        this.rigidBody = this.GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.xAxis = Input.GetAxis("Horizontal");   
+    }
+    private void FixedUpdate() {
+        //var position = this.rigidBody.position;
+        //position.x += this.xAxis * Time.deltaTime * speed;
+        //this.rigidBody.position = position;
+        var velocity = rigidBody.velocity;
+        velocity.x = this.speed * this.xAxis;
+        this.rigidBody.velocity = velocity;
+    }
+}
